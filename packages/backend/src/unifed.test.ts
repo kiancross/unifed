2118fac4d7f042eccc5aa707d3f5faa35ab7ef2d
Kian Cross <kian@kiancross.co.uk>
2020-11-02 12:27:56 +0000

    Add shared package for frontend and backend code (#22)

---
+++ packages/backend/src/unifed.test.ts
@@ -0,0 +1,13 @@
+/*
+ * CS3099 Group A3
+ */
+
+/*
+ * Just an example test to stop jest complaining.
+ * This file should probably be deleted once new
+ * tests have been added.
+ */
+
+test("example", () => {
+  expect(0).toBe(0);
+});
---
+++ packages/shared/jest.config.js
@@ -0,0 +1,14 @@
+/*
+ * CS3099 Group A3
+ */
+
+module.exports = {
+  roots: ["<rootDir>/src"],
+  testMatch: [
+    "**/__tests__/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)",
+  ],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest"
+  }
+};
