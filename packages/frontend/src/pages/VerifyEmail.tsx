dc3b2ed773c7317bec04a204b67b949cf6f957b9
Lewis Mazzei <lewis.mazzei@gmail.com>
2020-11-04 23:42:42 +0000

    redirect on success, notify user on failure

--- packages/frontend/src/pages/VerifyEmail.tsx
+++ packages/frontend/src/pages/VerifyEmail.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, Redirect, useParams } from "react-router-dom";
 import { passwordClient } from "../utils/accounts";
 
 interface VerifyEmailParams {
@@ -8,18 +8,27 @@ interface VerifyEmailParams {
 
 const VerifyEmail = (): JSX.Element => {
   const { token } = useParams<VerifyEmailParams>();
+  let verified = false;
+  try {
+    passwordClient.verifyEmail(token);
+    verified = true;
+  } catch (err) {
+    console.log(err);
+  }
   return (
-    <div>
-      <Link to="/">
-        <button
-          className="Submit-button"
-          onClick={() => {
-            passwordClient.verifyEmail(token);
-          }}
-        >
-          Verify Email
-        </button>
-      </Link>
+    <div className="container">
+      {verified ? (
+        // TODO redirect to home page on success
+        <Redirect to="/" />
+      ) : (
+        <React.Fragment>
+          <h1>Verification Failed!</h1>
+          <Link to="/">
+            <button className="Submit-button">Back to Login</button>
+          </Link>
+        </React.Fragment>
+      )}
+      ;
     </div>
   );
 };
