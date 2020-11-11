06384f42af58ddda73a460fd4575079da6e2ac1b
Lewis Mazzei <lewis.mazzei@gmail.com>
2020-11-11 23:28:29 +0000

    ResetPasswordReset.tsx validation

--- packages/frontend/src/pages/ResetPasswordRequest.tsx
+++ packages/frontend/src/pages/ResetPasswordRequest.tsx
@@ -3,38 +3,59 @@ import { Formik, Form, Field } from "formik";
 import "./../App.scss";
 import logo from "./../st-andrews-logo.png";
 import { passwordClient } from "../utils/accounts";
+import { Button, TextField } from "@material-ui/core";
 
-const submitButtonStyle = "Submit-button";
-
-interface FormValues {
+interface Values {
   email: string;
 }
 
+function validateEmail(email: string) {
+  const regexp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regexp.test(email);
+}
+
+function validate({ email }: Values) {
+  const errors: Partial<Values> = {};
+  if (!validateEmail(email)) {
+    errors.email = "Invalid email";
+  }
+  return errors;
+}
+
 const ResetPasswordRequest = (): JSX.Element => {
-  const initialValues = {
-    // TODO fill in if it was entered in login form
-    email: "",
-  };
   return (
     <div className="container">
       <img src={logo} alt="st andrews logo" width="250" height="300"></img>
-      <h1>Reset Password</h1>
       <Formik
-        initialValues={initialValues}
-        //TODO add validation
-        onSubmit={async (values: FormValues) => {
+        initialValues={{ email: "" }}
+        validate={validate}
+        validateOnBlur={true}
+        onSubmit={async (values: Values) => {
           await passwordClient.requestPasswordReset(values.email);
         }}
       >
-        <Form>
-          <div>
-            <label htmlFor="email">Email:</label>
-            <Field name="email" />
-          </div>
-          <button type="submit" className={submitButtonStyle}>
-            Send Email
-          </button>
-        </Form>
+        {({ errors, touched }) => (
+          <Form>
+            <div>
+              <Field
+                name="email"
+                as={TextField}
+                label="Email"
+                color="primary"
+                helperText={errors.email}
+                error={!!errors.email}
+              />
+            </div>
+            <Button
+              type="submit"
+              variant="contained"
+              style={{ margin: "20px" }}
+              disabled={!touched.email || !!errors.email}
+            >
+              Send Email
+            </Button>
+          </Form>
+        )}
       </Formik>
     </div>
   );
