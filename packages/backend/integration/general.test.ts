024cd8c7fad1a61d19c22d0b3e49de1e967873ff
Kian Cross <kian@kiancross.co.uk>
2020-11-13 18:21:41 +0000

    Fixed backend integration tests

--- packages/backend/integration/general.test.ts
+++ packages/backend/integration/general.test.ts
@@ -2,10 +2,10 @@
  * CS3099 Group A3
  */
 
-import { expect } from "chai";
+import test from "ava";
 import got from "got";
 
-it("coverage", async () => {
+test("coverage", async (t) => {
   const resp = await got("http://localhost:8080/internal/__coverage__");
-  expect(resp.statusCode).to.equal(200);
+  t.is(resp.statusCode, 200);
 });
