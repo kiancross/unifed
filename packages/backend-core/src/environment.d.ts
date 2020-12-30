40c64550cfefcf65a81b1e301b0be2f31686b48b
Kian Cross <kian@kiancross.co.uk>
2020-12-30 04:19:45 +0000

    Added env

---
+++ packages/backend-core/src/environment.d.ts
@@ -0,0 +1,27 @@
+/*
+ * CS3099 Group A3
+ */
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      UNIFED_SMTP_USERNAME?: string;
+      UNIFED_SMTP_PASSWORD?: string;
+      UNIFED_SMTP_HOST?: string;
+      UNIFED_SMTP_PORT?: string;
+      UNIFED_MONGO_HOSTNAME?: string;
+      UNIFED_MONGO_PORT?: string;
+      UNIFED_MONGO_DATABASE?: string;
+      UNIFED_MONGO_USERNAME?: string;
+      UNIFED_MONGO_PASSWORD?: string;
+      UNIFED_JWT_SECRET?: string;
+      UNIFED_SITE_HOST?: string;
+      UNIFED_SITE_PROTOCOL?: string;
+      UNIFED_APPLICATION_NAME?: string;
+      UNIFED_SERVER_PORT?: string;
+      NODE_ENV?: string;
+    }
+  }
+}
+
+export {};
