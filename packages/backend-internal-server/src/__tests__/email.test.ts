3f4cceb68f10271ff1b9cd705f32567604b64985
Kian Cross <kian@kiancross.co.uk>
2021-01-06 01:03:34 +0000

    Fixed testing errors

--- packages/backend-internal-server/src/__tests__/email.test.ts
+++ packages/backend-internal-server/src/__tests__/email.test.ts
@@ -2,10 +2,11 @@
  * CS3099 Group A3
  */
 
-process.env.SMTP_HOST = "smtp.ethereal.email";
-process.env.SMTP_PORT = "587";
-process.env.SMTP_USERNAME = "rogers.price3@ethereal.email";
-process.env.SMTP_PASSWORD = "YwgzQrmK2gemUfR63g";
+process.env.UNIFED_LOGGING_LEVEL = "info";
+process.env.UNIFED_SMTP_HOST = "smtp.ethereal.email";
+process.env.UNIFED_SMTP_PORT = "587";
+process.env.UNIFED_SMTP_USERNAME = "rogers.price3@ethereal.email";
+process.env.UNIFED_SMTP_PASSWORD = "YwgzQrmK2gemUfR63g";
 
 import test from "ava";
 import { emailTransporter } from "../email";
