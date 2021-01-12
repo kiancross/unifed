9ff5de07afc23251d53564cc982c28cb84dbdaed
Kian Cross <kian@kiancross.co.uk>
2021-01-12 17:39:10 +0000

    Lint the GraphQL queries made by the frontend (#149)

--- packages/frontend/.eslintrc.js
+++ packages/frontend/.eslintrc.js
@@ -5,12 +5,34 @@
 // Workaround for https://github.com/eslint/eslint/issues/3458
 require("@rushstack/eslint-patch/modern-module-resolution");
 
-module.exports = {
+// eslint-disable-next-line
+const fs = require("fs");
+
+const config = {
   extends: [
     "eslint:recommended",
     "react-app",
     "plugin:@typescript-eslint/recommended",
     "prettier/@typescript-eslint",
-    "plugin:prettier/recommended"
-  ]
+    "plugin:prettier/recommended",
+  ],
+  plugins: [],
+  rules: {},
+};
+
+const schemaPath = __dirname + "/.schema.graphql";
+
+if (fs.existsSync(schemaPath)) {
+  const schema = fs.readFileSync(schemaPath, "utf8");
+
+  config.plugins.push("graphql");
+  config.rules["graphql/template-strings"] = [
+    "error",
+    {
+      env: "apollo",
+      schemaString: schema,
+    },
+  ];
 }
+
+module.exports = config;
