b80382278bf4eaefac062c41fd57c7312d12757d
Kian Cross <kian@kiancross.co.uk>
2021-01-14 19:51:10 +0000

    Renamed

--- packages/backend-internal-server/src/index.ts
+++ packages/backend-internal-server/src/index.ts
@@ -8,26 +8,17 @@ import mongoose from "mongoose";
 import { config, logger } from "@unifed/backend-core";
 import { routes } from "./routes";
 
-(async () => {
-  const mongoOptions = { useNewUrlParser: true, useUnifiedTopology: true };
-  const mongoUri =
-    `mongodb://${config.mongoUsername}:${config.mongoPassword}@` +
-    `${config.mongoHostname}:${config.mongoPort}/${config.mongoDatabase}`;
+const mongoOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+const mongoUri =
+  `mongodb://${config.mongoUsername}:${config.mongoPassword}@` +
+  `${config.mongoHostname}:${config.mongoPort}/${config.mongoDatabase}`;
 
+(async () => {
   await mongoose.connect(mongoUri, mongoOptions);
 
   logger.info("Connected to database");
 
   const app = express();
-
-  /* istanbul ignore next */
-  if (global.__coverage__) {
-    logger.info("Serving coverage report");
-
-    app.get("/internal/__coverage__", (_, res) => {
-      res.json({ coverage: global.__coverage__ });
-    });
-  }
 
   app.use("/", await routes);
 
