07ec6d4c2aabc9761dfb53f23d2c3fd54d40e87d
Rob <rmardall@gmail.com>
2021-01-18 15:56:30 +0000

    add error boundary

--- packages/frontend/src/components/ErrorBoundary/ErrorBoundary.js
+++ packages/frontend/src/components/ErrorBoundary/ErrorBoundary.js
@@ -1,34 +1,22 @@
 import React from "react";
-import { Button, Container, Typography } from "@material-ui/core";
-import styles from "../../pages/PageNotFoundPage/PageNotFoundPage.module.scss";
 
 class ErrorBoundary extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { error: null, errorInfo: null };
+    this.state = { hasError: false };
   }
 
-  componentDidCatch(error, errorInfo) {
-    // Catch errors in any components below and re-render with error message
-    this.setState({
-      error: error,
-      errorInfo: errorInfo,
-    });
-    // You can also log error messages to an error reporting service here
+  componentDidCatch(error, info) {
+    // Display fallback UI
+    this.setState({ hasError: true });
   }
 
   render() {
-    return (
-      <Container className={styles.container}>
-        <Typography variant="h6" gutterBottom>
-          There was an error when carrrying out the request. Please try again and if the error
-          persists, let us know and we will fix it!
-        </Typography>
-        <Button color="primary" variant="contained" href="/">
-          Return Home
-        </Button>
-      </Container>
-    );
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <h1>Something went wrong.</h1>;
+    }
+    return this.props.children;
   }
 }
 
