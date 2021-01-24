31df22272e72014386cd91141aab1a5212615cfe
Rob <rmardall@gmail.com>
2021-01-24 19:57:54 +0000

    Add Card Header

--- packages/frontend/src/pages/PostPage/Post.tsx
+++ packages/frontend/src/pages/PostPage/Post.tsx
@@ -3,8 +3,18 @@
  */
 
 import React from "react";
-import { Card, CardContent, Grid, Link, Typography } from "@material-ui/core";
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  Grid,
+  IconButton,
+  Link,
+  Typography,
+} from "@material-ui/core";
 import MarkdownViewer from "../../components/MarkdownViewer";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import style from "./PostPage.module.scss";
 
 interface PostValues {
   username: string;
@@ -13,13 +23,33 @@ interface PostValues {
 }
 
 const Post = (props: PostValues): JSX.Element => {
+  //const [menuOpen, setMenuOpen] = React.useState(null);
+  //const open = Boolean(menuOpen);
+
+  const handleClick = () => {
+    console.log("clicked");
+  };
+  /*
+  const handleClose = () => {
+    setMenuOpen(null);
+  };*/
+
   return (
     <Grid item xs={12}>
       <Card style={{ textAlign: "left" }}>
-        <CardContent>
-          <Typography variant="body2" gutterBottom>
-            <Link href={"/user/" + props.username}>{props.username}</Link>
-          </Typography>
+        <CardHeader
+          action={
+            <IconButton color="inherit" edge="end" size="small" onClick={handleClick}>
+              <MoreVertIcon />
+            </IconButton>
+          }
+          title={
+            <Typography variant="h5" gutterBottom>
+              <Link href={"/user/" + props.username}>{props.username}</Link>
+            </Typography>
+          }
+        />
+        <CardContent className={style.cardContent}>
           <Typography variant="h6">{props.title ? props.title : "Comment"}</Typography>
           <Typography variant="body2">
             <MarkdownViewer>{props.text}</MarkdownViewer>
