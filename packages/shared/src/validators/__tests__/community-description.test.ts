50e7a47d69337c2a30bb74785dc93e831b90c8da
Kian Cross <kian@kiancross.co.uk>
2021-02-14 22:42:17 +0000

    Added validator tests

---
+++ packages/shared/src/validators/__tests__/community-description.test.ts
@@ -0,0 +1,30 @@
+/*
+ * CS3099 Group A3
+ */
+
+import test from "ava";
+import { validateCommunityDescription } from "../community-description";
+
+test("Empty", (t) => {
+  t.false(validateCommunityDescription(""));
+});
+
+test("1 character (boundary)", (t) => {
+  t.true(validateCommunityDescription("a"));
+});
+
+test("128 characters (boundary)", (t) => {
+  t.true(validateCommunityDescription("a".repeat(128)));
+});
+
+test("129 characters (too long)", (t) => {
+  t.false(validateCommunityDescription("a".repeat(129)));
+});
+
+test("Example", (t) => {
+  t.true(
+    validateCommunityDescription(
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    ),
+  );
+});
--- packages/shared/src/validators/communityDescription.ts
+++
@@ -1,7 +0,0 @@
-/*
- * CS3099 Group A3
- */
-
-export function validateCommunityDescription(communityDescription: string): boolean {
-  return /^.{1,128}$/.test(communityDescription);
-}
