2f2e91a50a62208726915ad40e7e9732778f3520
Kian Cross <kian@kiancross.co.uk>
2021-02-16 02:54:33 +0000

    Added link components

---
+++ packages/frontend/src/components/Links/index.ts
@@ -0,0 +1,9 @@
+/*
+ * CS3099 Group A3
+ */
+
+import Link from "./Link";
+import ButtonLink from "./ButtonLink";
+import IconButtonLink from "./IconButtonLink";
+
+export { Link, ButtonLink, IconButtonLink };
