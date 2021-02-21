c70249326d5a932ee6b02fe93d82e8a33eabba35
rmardall <60814790+rmardall@users.noreply.github.com>
2021-02-21 14:54:50 +0000

    Fixed user settings page and privacy policy link in dark mode (#250)
    * Changed dark mode colours of profile settings page
    * Changed colour of privacy policy link

--- packages/frontend/src/components/App/Footer.tsx
+++ packages/frontend/src/components/App/Footer.tsx
@@ -3,15 +3,24 @@
  */
 
 import React, { ReactElement } from "react";
-import { Box, Container, Paper } from "@material-ui/core";
+import { Box, Container, makeStyles, Paper } from "@material-ui/core";
 import { Link } from "../Links";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    color: theme.palette.text.primary,
+  },
+}));
+
 const Footer = (): ReactElement => {
+  const classes = useStyles();
   return (
     <Box paddingTop="3rem">
       <Paper elevation={0} style={{ textAlign: "left", padding: "1.5rem" }}>
         <Container maxWidth="lg">
-          <Link to="/privacy-notice">Privacy Notice</Link>
+          <Link classes={classes} to="/privacy-notice">
+            Privacy Notice
+          </Link>
         </Container>
       </Paper>
     </Box>
