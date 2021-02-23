9100a30f38a95dd6d8114ac696c5674e790a087a
Rob <rmardall@gmail.com>
2021-02-23 16:42:42 +0000

    added UserProfilePage tests

---
+++ packages/frontend/src/pages/UserProfilePage/UserProfilePage.test.tsx
@@ -0,0 +1,123 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import { AllTheProviders } from "../../helpers/test";
+import React from "react";
+import UserProfilePage, { GET_POSTS } from "./UserProfilePage";
+
+const host = "this";
+const community = "all";
+const user = "testuser";
+const title = "Test Title";
+
+test("UserProfilePage posts render with correct user", async () => {
+  const getPostsMock = [
+    {
+      request: {
+        query: GET_POSTS,
+        variables: {
+          community: community,
+          host: host,
+        },
+      },
+      result: {
+        data: {
+          getPosts: [
+            {
+              id: "001",
+              title: title,
+              host: host,
+              author: {
+                id: user,
+              },
+              body: "Test Body",
+            },
+          ],
+        },
+      },
+    },
+  ];
+
+  render(
+    <AllTheProviders
+      path="/user/:username"
+      initialEntries={["/user/testuser"]}
+      mocks={getPostsMock}
+    >
+      <UserProfilePage />
+    </AllTheProviders>,
+  );
+
+  await waitFor(() => {
+    expect(screen.getByText("Test Title"));
+  });
+});
+
+test("UserProfilePage post does not render with incorrect user", async () => {
+  const getPostsMock = [
+    {
+      request: {
+        query: GET_POSTS,
+        variables: {
+          community: community,
+          host: host,
+        },
+      },
+      result: {
+        data: {
+          getPosts: [
+            {
+              id: "001",
+              title: title,
+              host: host,
+              author: {
+                id: "invalidUser",
+              },
+              body: "Test Body",
+            },
+          ],
+        },
+      },
+    },
+  ];
+
+  render(
+    <AllTheProviders
+      path="/user/:username"
+      initialEntries={["/user/testuser"]}
+      mocks={getPostsMock}
+    >
+      <UserProfilePage />
+    </AllTheProviders>,
+  );
+
+  await waitFor(() => {
+    expect(screen.queryByText("Test Title")).toBeNull;
+  });
+});
+
+test("UserProfilePage post does not render with incorrect user", async () => {
+  const getPostsMock = [
+    {
+      request: {
+        query: GET_POSTS,
+        variables: {
+          id: community,
+          host: host,
+        },
+      },
+    },
+  ];
+
+  render(
+    <AllTheProviders
+      path="/user/:username"
+      initialEntries={["/user/testuser"]}
+      mocks={getPostsMock}
+    >
+      <UserProfilePage />
+    </AllTheProviders>,
+  );
+
+  await waitFor(() => {
+    expect(screen.getByText("Error!"));
+  });
+});
