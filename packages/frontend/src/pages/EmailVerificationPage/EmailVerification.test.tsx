fbd9b278205d9ec42a1a9992552f67961f13f60c
Rob <rmardall@gmail.com>
2021-02-23 12:48:38 +0000

    added EmailVerification test

---
+++ packages/frontend/src/pages/EmailVerificationPage/EmailVerification.test.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import EmailVerificationPage from "./EmailVerificationPage";
+import { AllTheProviders } from "../../helpers/test";
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+//For some reason the test was not finding the route without BrowserRouter
+test("EmailVerification Renders", async () => {
+  const homeMessage = "Home Page";
+  render(
+    <AllTheProviders path="/verify-email/:token" initialEntries={["/verify-email/123"]}>
+      <BrowserRouter>
+        <EmailVerificationPage />
+        <Route path="/">{homeMessage}</Route>
+      </BrowserRouter>
+    </AllTheProviders>,
+  );
+
+  expect(screen.getByText(homeMessage));
+});
