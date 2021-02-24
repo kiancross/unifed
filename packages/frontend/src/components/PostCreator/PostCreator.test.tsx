5a6ea8d4efd4aa41b8d3e967a746c67e3f47a8fa
Rob <rmardall@gmail.com>
2021-02-24 16:45:33 +0000

    tidied up PostCreator

--- packages/frontend/src/components/PostCreator/PostCreator.test.tsx
+++ packages/frontend/src/components/PostCreator/PostCreator.test.tsx
@@ -1,4 +1,4 @@
-import { getByText, render, screen, waitFor } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import React from "react";
 import { AllTheProviders } from "../../helpers/test";
 import PostCreator, { createPostQuery } from "./PostCreator";
@@ -9,45 +9,9 @@ const community = "all";
 const server = "this";
 const mockFunc = jest.fn();
 const submitButtonText = "submit button";
-
-test("PostCreator renders", async () => {
-  const createPostMock = [
-    {
-      request: {
-        query: createPostQuery,
-        variables: {
-          title: "Test Title",
-          body: "Test Body",
-          community: community,
-          host: server,
-          parentPost: "123",
-        },
-      },
-      result: {
-        data: {
-          createPost: {
-            id: "001",
-          },
-        },
-      },
-    },
-  ];
-
-  render(
-    <AllTheProviders mocks={createPostMock}>
-      <PostCreator
-        submitButtonText={submitButtonText}
-        onSuccess={mockFunc}
-        server={server}
-        community={community}
-      />
-    </AllTheProviders>,
-  );
-
-  await waitFor(() => {
-    userEvent.click(screen.getByText(submitButtonText));
-  });
-});
+const title = "Test title";
+const body = "Test body";
+const parentPost = "123";
 
 test("PostCreator error renders", async () => {
   const createPostMockError = [
@@ -55,11 +19,11 @@ test("PostCreator error renders", async () => {
       request: {
         query: createPostQuery,
         variables: {
-          title: undefined,
-          body: "Test Body",
+          title: title,
+          body: body,
           community: community,
           host: server,
-          parentPost: "123",
+          parentPost: parentPost,
         },
       },
       result: {
@@ -74,6 +38,7 @@ test("PostCreator error renders", async () => {
         onSuccess={mockFunc}
         server={server}
         community={community}
+        parentId={parentPost}
       />
     </AllTheProviders>,
   );
