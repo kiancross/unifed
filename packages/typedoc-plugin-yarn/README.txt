83275c5c1cc19505f7786e41fadb37dbc6a3ba6b
Kian Cross <kian@kiancross.co.uk>
2021-03-17 02:25:53 +0000

    Updated

--- packages/typedoc-plugin-yarn/README.txt
+++
@@ -1,4 +0,0 @@
-Plugin taken from here:
-https://github.com/yarnpkg/berry/tree/master/packages/gatsby/typedoc-plugin-yarn
-
-Used to ensure virtual references are not included in the documentation.
--- typedoc.js
+++ typedoc.js
@@ -5,8 +5,6 @@
 // Derived from
 // https://github.com/yarnpkg/berry/blob/master/packages/gatsby/typedoc.js
 
-const path = require("path");
-
 /**
  * @type {import('typedoc').TypeDocOptions}
  */
@@ -24,43 +22,7 @@ module.exports = {
 
   excludeExternals: true,
 
-  theme: `${path.dirname(
-    require.resolve(`typedoc-neo-theme/package.json`)
-  )}/bin/default`,
-
-  plugin: [
-    "typedoc-plugin-yarn",
-    "typedoc-neo-theme",
-    "@strictsoftware/typedoc-plugin-monorepo",
-  ],
+  plugin: ["@strictsoftware/typedoc-plugin-monorepo"],
 
   "external-modulemap": ".*packages/([^/]+)/.*",
-
-  source: [
-    {
-      path: "https://github.com/kiancross/unifed/tree/master/",
-      line: "L",
-    },
-  ],
-
-  outline: [
-    {
-      Packages: {
-        "@unifed/shared": "shared",
-        "@unifed/frontend": "frontend",
-        "@unifed/backend-ml": "backend_ml",
-        "@unifed/backend-core": "backend_core",
-        "@unifed/backend-federation-client": "backend_federation_client",
-        "@unifed/backend-federation-server": "backend_federation_server",
-        "@unifed/backend-internal-server": "backend_internal_server",
-      },
-    },
-  ],
-
-  links: [
-    {
-      label: "Developer Documentation",
-      url: "https://kiancross.github.io/unifed/",
-    },
-  ],
 };
