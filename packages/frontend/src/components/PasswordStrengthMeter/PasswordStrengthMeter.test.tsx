ff142632b479db719c3c7da48de37db82e60b62b
Lewis Mazzei <lewis.mazzei@gmail.com>
2021-03-18 01:14:30 +0000

    add tests

---
+++ packages/frontend/src/components/PasswordStrengthMeter/PasswordStrengthMeter.test.tsx
@@ -0,0 +1,42 @@
+import { render } from "@testing-library/react";
+import PasswordStrengthMeter from "./PasswordStrengthMeter";
+
+test("correct meter value and label when password is empty string", () => {
+    const password = "";
+
+    const { getByText, getByTestId } = render(
+      <PasswordStrengthMeter password={password}/>
+    );
+
+    getByText("very weak");
+});
+
+test("correct label when password is very weak", () => {
+    const password = "password";
+
+    const { getByText } = render(
+      <PasswordStrengthMeter password={password}/>
+    );
+
+    getByText("very weak");
+});
+
+test("correct label when strength is good", () => {
+    const password = "ThisIsGood!";
+
+    const { getByText } = render(
+      <PasswordStrengthMeter password={password}/>
+    );
+
+    getByText("good");
+});
+
+test("correct label when strength is strong", () => {
+    const password = "ThisIsStrong!";
+
+    const { getByText } = render(
+      <PasswordStrengthMeter password={password}/>
+    );
+
+    getByText("strong");
+});
