90517e696f24c76d29e2b6f1aa408a9140d264e1
Lewis Mazzei <lewis.mazzei@gmail.com>
2021-03-18 19:56:23 +0000

    add warning and suggestions via tooltip

--- packages/frontend/src/components/PasswordStrengthMeter/PasswordStrengthMeter.tsx
+++ packages/frontend/src/components/PasswordStrengthMeter/PasswordStrengthMeter.tsx
@@ -1,8 +1,24 @@
 import { validatePassword } from "@unifed/shared";
+import { Tooltip } from "@material-ui/core";
+import { PasswordValidationResult } from "@unifed/shared/src/validators/password";
 
 interface Props {
   password: string;
 }
+
+const tooltipTitle = (result: PasswordValidationResult) => {
+  const suggestions = result.suggestions;
+  const warning = result.warning;
+  // prevent empty tooltip from appearing
+  if (suggestions.length == 0) {
+    return "";
+  }
+  if (warning != "") {
+    return warning + ". " + suggestions[0];
+  } else {
+    return suggestions[0];
+  }
+};
 
 const PasswordStrengthMeter = (props: Props): JSX.Element => {
   const result = validatePassword(props.password);
@@ -23,9 +39,11 @@ const PasswordStrengthMeter = (props: Props): JSX.Element => {
         data-testid="meter"
         style={{ width: "100%" }}
       />
-      <p style={{ margin: "0", textAlign: "center", fontSize: "0.8rem" }}>
-        <b>Password Strength:</b> {strengthLabel[result.score]}
-      </p>
+      <Tooltip title={tooltipTitle(result)}>
+        <p style={{ margin: "0", textAlign: "center", fontSize: "0.8rem" }}>
+          <b>Password Strength:</b> {strengthLabel[result.score]}
+        </p>
+      </Tooltip>
     </div>
   );
 };
