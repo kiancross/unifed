33b801407e2548a324c048a9dbd2ba97ced70139
Lewis Mazzei <lewis.mazzei@gmail.com>
2021-03-18 15:19:56 +0000

    add score tests to password.test.ts

--- packages/shared/src/validators/__tests__/password.test.ts
+++ packages/shared/src/validators/__tests__/password.test.ts
@@ -9,8 +9,10 @@ test("Weak", (t) => {
   const result = validatePassword("weak");
 
   t.false(result.valid);
-  t.not(result.warning, undefined);
 
+  t.true(result.score < 3)
+
+  t.not(result.warning, undefined);
   if (result.warning) {
     t.regex(result.warning, /^.*$/);
   }
@@ -24,6 +26,7 @@ test("Strong", (t) => {
   const result = validatePassword("ThisIsAStr0ngP@55w0rd");
 
   t.true(result.valid);
+  t.true(result.score >= 3)
   t.is(result.warning, undefined);
   t.is(result.suggestions.length, 0);
 });
