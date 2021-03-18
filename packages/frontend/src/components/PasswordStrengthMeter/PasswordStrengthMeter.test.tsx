288dc4f8438f9162cad469789518e158d2ad540c
Lewis Mazzei <lewis.mazzei@gmail.com>
2021-03-18 20:11:18 +0000

    refactor tests

--- packages/frontend/src/components/PasswordStrengthMeter/PasswordStrengthMeter.test.tsx
+++ packages/frontend/src/components/PasswordStrengthMeter/PasswordStrengthMeter.test.tsx
@@ -2,41 +2,33 @@ import { render } from "@testing-library/react";
 import PasswordStrengthMeter from "./PasswordStrengthMeter";
 
 test("correct meter value and label when password is empty string", () => {
-    const password = "";
+  const password = "";
 
-    const { getByText, getByTestId } = render(
-      <PasswordStrengthMeter password={password}/>
-    );
+  const { getByText } = render(<PasswordStrengthMeter password={password} />);
 
-    getByText("very weak");
+  getByText("very weak");
 });
 
 test("correct label when password is very weak", () => {
-    const password = "password";
+  const password = "password";
 
-    const { getByText } = render(
-      <PasswordStrengthMeter password={password}/>
-    );
+  const { getByText } = render(<PasswordStrengthMeter password={password} />);
 
-    getByText("very weak");
+  getByText("very weak");
 });
 
 test("correct label when strength is good", () => {
-    const password = "ThisIsGood!";
+  const password = "ThisIsGood!";
 
-    const { getByText } = render(
-      <PasswordStrengthMeter password={password}/>
-    );
+  const { getByText } = render(<PasswordStrengthMeter password={password} />);
 
-    getByText("good");
+  getByText("good");
 });
 
 test("correct label when strength is strong", () => {
-    const password = "ThisIsStrong!";
+  const password = "ThisIsStrong!";
 
-    const { getByText } = render(
-      <PasswordStrengthMeter password={password}/>
-    );
+  const { getByText } = render(<PasswordStrengthMeter password={password} />);
 
-    getByText("strong");
+  getByText("strong");
 });
