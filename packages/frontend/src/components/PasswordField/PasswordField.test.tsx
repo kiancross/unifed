211851f992e07555b83efd36ccfd21cae84da0ea
Lewis Mazzei <lewis.mazzei@gmail.com>
2021-03-24 21:30:07 +0000

    add simple test

---
+++ packages/frontend/src/components/PasswordField/PasswordField.test.tsx
@@ -0,0 +1,10 @@
+/*
+ * CS3099 Group A3
+ */
+
+import { render } from "@testing-library/react";
+import { PasswordField } from "./PasswordField";
+
+test("PasswordField renders", () => {
+  render(<PasswordField />);
+});
