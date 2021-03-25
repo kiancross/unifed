ec371ac29b578b49d048475d2f63c7ae809a01cc
Lewis Mazzei <lewis.mazzei@gmail.com>
2021-03-25 14:17:53 +0000

    fix style

--- packages/shared/src/validators/__tests__/password.test.ts
+++ packages/shared/src/validators/__tests__/password.test.ts
@@ -10,7 +10,7 @@ test("Weak", (t) => {
 
   t.false(result.valid);
 
-  t.true(result.score < 3)
+  t.true(result.score < 3);
 
   t.not(result.warning, undefined);
   if (result.warning) {
@@ -26,7 +26,7 @@ test("Strong", (t) => {
   const result = validatePassword("ThisIsAStr0ngP@55w0rd");
 
   t.true(result.valid);
-  t.true(result.score >= 3)
+  t.true(result.score >= 3);
   t.is(result.warning, undefined);
   t.is(result.suggestions.length, 0);
 });
