1c6daade002f696ae19c0ee99846461e85839cfd
Rob <rmardall@gmail.com>
2021-03-31 18:56:10 +0100

    made UserIcon unclickable in header

--- packages/frontend/src/components/UserIcon/UserIcon.tsx
+++ packages/frontend/src/components/UserIcon/UserIcon.tsx
@@ -9,6 +9,7 @@ import { Link as RouterLink } from "react-router-dom";
 interface Props {
   username: string;
   small?: boolean;
+  inHeader?: boolean;
 }
 
 const useStyles = makeStyles<Theme, Props>((theme) => ({
@@ -24,13 +25,19 @@ const useStyles = makeStyles<Theme, Props>((theme) => ({
 export const UserIcon = (props: Props): ReactElement => {
   const classes = useStyles(props);
 
-  return (
+  const avatar = (
+    <Avatar alt={props.username} className={classes.avatar}>
+      <Typography variant={props.small ? "body2" : "h6"} className={classes.text}>
+        {props.username.charAt(0).toUpperCase()}
+      </Typography>
+    </Avatar>
+  );
+
+  return props.inHeader ? (
+    avatar
+  ) : (
     <CardActionArea to={"/user/" + props.username} component={RouterLink}>
-      <Avatar alt={props.username} className={classes.avatar}>
-        <Typography variant={props.small ? "body2" : "h6"} className={classes.text}>
-          {props.username.charAt(0).toUpperCase()}
-        </Typography>
-      </Avatar>
+      {avatar}
     </CardActionArea>
   );
 };
