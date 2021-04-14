327ec359ba6820a071b4306ce8c0a84557b6db63
Allan <allanmathew1@gmail.com>
2021-04-15 00:45:37 +0100

    action button tests started

---
+++ packages/frontend/src/components/ActionButton/ActionButton.test.tsx
@@ -0,0 +1,15 @@
+/*
+ * CS3099 Group A3
+ */
+
+// import { render } from "@testing-library/react";
+// import { BrowserMockProvider } from "../../helpers";
+// import { ActionButton } from "./ActionButton";
+
+// test("Render ActionButton on loading", async () => {
+//   const { getByText } = render(<ActionButton loading={true} error={undefined} />);
+//   getByText("Homepage");
+//   await waitFor(() => {
+//     screen.getByRole("button", { name: "Create" });
+//   });
+// });
