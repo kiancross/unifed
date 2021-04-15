ae7b3e83d6859518849c01330cfc1693722a62eb
Rob <rmardall@gmail.com>
2021-04-15 17:07:36 +0100

    added test for getAllPosts

--- packages/backend-internal-server/src/resolvers/__tests__/users.test.ts
+++ packages/backend-internal-server/src/resolvers/__tests__/users.test.ts
@@ -7,10 +7,11 @@ import test from "ava";
 import { graphql } from "graphql";
 import { Container } from "typedi";
 
-import { setup, generateUser } from "@unifed/backend-testing";
+import { setup, generateUser, generatePost } from "@unifed/backend-testing";
 import { RemoteReference, UserModel } from "@unifed/backend-core";
 
 import { getMergedSchema } from "../../schema";
+import nock from "nock";
 
 const schema = (async () => getMergedSchema(Container.of()))();
 
@@ -173,20 +174,31 @@ test.serial("unsubscribe", async (t) => {
   t.is(response.data.getSubscriptions.length, 0);
 });
 
-test.serial("getAllPosts gets posts", async (t) => {
+test.serial("getAllPosts", async (t) => {
   const postReference: RemoteReference = new RemoteReference();
   postReference.host = "thishost";
   postReference.id = "testid";
 
+  const post = generatePost("all");
+  post.id = "testid";
+  post.host = "thishost";
+
+  const scope = nock("http://thishost").get("/fed/posts/testid").reply(200, post);
+
   const user = generateUser();
+  user.username = "testuser";
   user.posts = [postReference];
+  await UserModel.create(user);
+
+  await UserModel.findOne({ username: "testuser" });
 
   let response = await graphql(
     await schema,
     `
       query {
-        getAllPosts {
-          id
+        getAllPosts(username: "testuser") {
+          title
+          body
         }
       }
     `,
@@ -197,4 +209,13 @@ test.serial("getAllPosts gets posts", async (t) => {
       },
     },
   );
+
+  if (!response.data) {
+    t.fail();
+    return;
+  }
+
+  t.truthy(response.data.getAllPosts);
+
+  scope.done();
 });
