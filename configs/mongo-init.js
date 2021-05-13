074aaa5ff84bd75680150acc3ac603bfc44f3c5b
Kian Cross <kian@kiancross.co.uk>
2021-05-13 04:28:23 +0100

    Added license to base files

--- configs/mongo-init.js
+++ configs/mongo-init.js
@@ -1,5 +1,22 @@
 /*
- * CS3099 Group A3
+ * Copyright (C) 2020 Kian Cross
+ * Copyright (C) 2020 Allan Mathew Chacko
+ * Copyright (C) 2020 Robert Mardall
+ *
+ * This file is part of Unifed.
+ *
+ * Unifed is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Unifed is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Unifed.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 db.createUser({
