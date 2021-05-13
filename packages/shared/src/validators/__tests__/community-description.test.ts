fea37a04dee277cbb933db2ec6ff28acc0d79c28
Kian Cross <kian@kiancross.co.uk>
2021-05-13 04:38:34 +0100

    Added license headers to `shared`

--- packages/shared/src/validators/__tests__/community-description.test.ts
+++ packages/shared/src/validators/__tests__/community-description.test.ts
@@ -1,5 +1,21 @@
 /*
- * CS3099 Group A3
+ * Copyright (C) 2021 Kian Cross
+ * Copyright (C) 2021 Lewis Mazzei
+ *
+ * This file is part of Unifed.
+ *
+ * Unifed is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Unifed is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Unifed.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 import test from "ava";
